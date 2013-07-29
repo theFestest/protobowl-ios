2d29c3b94d2642eddbe3fe6775718c5f93394188
Donald Pinckney <tootsiejasmine@gmail.com>
2013-07-28 23:25:40 -0700

    Setup classes to begin work on menu view controller

---
+++ Protobowl/SideMenuViewController.h
@@ -0,0 +1,18 @@
+//
+//  SideMenuViewController.h
+//  Protobowl
+//
+//  Created by Donald Pinckney on 7/20/13.
+//  Copyright (c) 2013 Donald Pinckney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MainViewController.h"
+
+@interface SideMenuViewController : UIViewController<UITableViewDataSource, UITableViewDelegate, ProtobowlLeaderboardDelegate, UITextFieldDelegate>
+
+@property (nonatomic, weak) MainViewController *mainViewController;
+
+- (void) setFullyOnscreen:(BOOL) onscreen;
+
+@end
