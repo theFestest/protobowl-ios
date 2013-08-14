55e5aafd15c91b06744ca2310fe50401634d0508
Donald Pinckney <donald_pinckney@icloud.com>
2013-08-13 22:40:47 -0700

    Did initial implementation of split view controller for iPad UI

--- Protobowl/MainViewController.h
+++ Protobowl/MainViewController.h
@@ -9,6 +9,6 @@
 #import <UIKit/UIKit.h>
 #import "ProtobowlConnectionManager.h"
 
-@interface MainViewController : UIViewController <ProtobowlRoomDelegate, UIScrollViewDelegate, UIGestureRecognizerDelegate>
+@interface MainViewController : UIViewController <ProtobowlRoomDelegate, UIScrollViewDelegate, UIGestureRecognizerDelegate, UISplitViewControllerDelegate>
 @property (nonatomic, strong) ProtobowlConnectionManager *manager;
 @end
--- Protobowl/MainViewController.m
+++ Protobowl/MainViewController.m
@@ -52,6 +52,8 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *buzzLogTableView;
 @property (nonatomic, strong) LinedTableViewController *buzzLogController;
+
+@property (strong, nonatomic) UIPopoverController *masterPopoverController;
 @end
 
 @implementation MainViewController
@@ -482,6 +484,22 @@
         return YES;
     }
     return NO;
+}
+
+
+#pragma mark - Split View Controller Delegate Method Implementations
+- (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
+{
+    barButtonItem.title = @"Room Info";
+    [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
+    self.masterPopoverController = popoverController;
+}
+
+- (void)splitViewController:(UISplitViewController *)splitController willShowViewController:(UIViewController *)viewController invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem
+{
+    // Called when the view is shown again in the split view, invalidating the button and popover controller.
+    [self.navigationItem setLeftBarButtonItem:nil animated:YES];
+    self.masterPopoverController = nil;
 }
 
 @end
